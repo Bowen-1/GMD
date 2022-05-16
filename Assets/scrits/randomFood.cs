6fa070afe00c65f8b0607667bf95cd726c748ce3
Bowen-1 <bowen19990924@gmail.com>
2022-05-16 22:40:14 +0200

    ADD

---
+++ Assets/scrits/randomFood.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomFood : MonoBehaviour
+{
+
+    float timer;//食物生成间隔
+
+    public GameObject[] foods;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;//随时间增加
+        //生成物体
+        if (timer > 3f)
+        {
+           GameObject food= Instantiate(foods[Random.Range(0, foods.Length)], this.transform);
+            food.transform.position = new Vector3(Random.Range(9.5f, 19f), -1);
+            GameObject food2 = Instantiate(foods[Random.Range(0, foods.Length)], this.transform);
+            food2.transform.position = new Vector3(Random.Range(9.5f, 19f), -1.86f);
+            GameObject food3 = Instantiate(foods[Random.Range(0, foods.Length)], this.transform);
+            food3.transform.position = new Vector3(Random.Range(9.5f, 19f), -2.79f);
+            timer = 0;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# GMD
-GMD
+# GMDProject
